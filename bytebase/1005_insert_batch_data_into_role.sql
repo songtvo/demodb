c6b45305280652c03e57fcb4f5d4aac51ee5dd5a
steve.svt <steve.svt@byteuptech.com>
2024-07-31 17:47:26 +0700

    correct scripting

--- bytebase/1005_insert_batch_data_into_role.sql
+++ bytebase/1005_insert_batch_data_into_role.sql
@@ -1,3 +1,3 @@
-INSERT INTO `role`(`name`, created_by, updated_by) VALUES ('Admin 1', 1, 1);
-INSERT INTO `role`(`name`, created_by, updated_by) VALUES ('Admin 2', 1, 1);
-INSERT INTO `role`(`name`, created_by, updated_by) VALUES ('Admin 3', 1, 1);
+INSERT INTO `role`(`name`, created_by, updated_by, `desc`) VALUES ('Admin 1', 1, 1, 'A');
+INSERT INTO `role`(`name`, created_by, updated_by, `desc`) VALUES ('Admin 2', 1, 1, 'a');
+INSERT INTO `role`(`name`, created_by, updated_by, `desc`) VALUES ('Admin 3', 1, 1, 'c');
